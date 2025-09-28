9fe6ad74ebdc29496ab339934ff4fb0bbc4cd8c5
sean-theo <sean.theo@outlook.com>
2025-09-28 10:13:10 -0400

    Initial commit, created files to match UML with classes and methods

---
+++ Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    internal class Inventory
+    {
+        public static BindingList<Product> Products { get; set; } = new BindingList<Product>();
+        public static BindingList<Part> AllParts { get; set; } = new BindingList<Part>();
+        
+        public static void AddProduct(Product product)
+        {
+        }
+        public static bool RemoveProduct(int productID)
+        {
+            return false;
+        }
+        public static Product LookupProduct(int productID)
+        {
+            return null;
+        }
+        public static void UpdateProduct(int productID, Product product)
+        {
+        }
+        public static void AddPart(Part part)
+        {
+        }
+        public static bool DeletePart(Part part)
+        {
+            return false;
+        }
+        public static Part LookUpPart(int PartID)
+        {
+            return null;
+        }
+        public static void UpdatePart(int partID, Part part)
+        {
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    public class Product
+    {
+        public string ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+
+        public BindingList<Part> AssociatedParts { get; set; } = new BindingList<Part>();
+
+
+        public void AddAssociatedPart(Part part)
+        {
+        }
+        public bool RemoveAssociatedPart(int partID)
+        {
+            return false;
+        }
+        public Part LookupAssociatedPart(int partID)
+        {
+            return null;
+        }
+    }
+}
